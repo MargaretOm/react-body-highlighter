a50a141cf18b4683e128ca1f2c943665a76dba19
Margarita Omelchuk <margarita.omelchuk@rubyroidlabs.com>
2023-05-11 16:55:37 +0200

    Add left and right muscles.

--- example/index.tsx
+++ example/index.tsx
@@ -6,13 +6,16 @@ import ProductionModel from 'react-body-highlighter';
 import Model from '../src/component/Model';
 
 import { IExerciseData, IMuscleStats } from '../src/component/metadata';
+import { useState } from 'react';
 
 function App() {
   const [exampleState, setExampleState] = React.useState(false); // Model should not re-render when parent component re-renders
   const [data, setData] = React.useState<IExerciseData[]>([
-    { name: 'Bench Press', muscles: ['chest', 'triceps', 'front-deltoids'] },
-    { name: 'Tricep Pushdown', muscles: ['triceps'] },
+    { name: 'Bench Press', muscles: ['chest', 'left-tricep', 'left-shoulder'] },
+    { name: 'Tricep Pushdown', muscles: ['left-tricep'] },
   ]);
+
+  const [type, setType] = useState<'posterior' | 'anterior'>('anterior');
 
   const handleClick = React.useCallback(({ muscle, data }: IMuscleStats) => {
     const { exercises, frequency } = data;
@@ -28,8 +31,10 @@ function App() {
   return (
     <>
       <div style={styles.container}>
-        <Model data={data} onClick={handleClick} bodyColor="#ccc" />
-        <ProductionModel type="posterior" data={data} highlightedColors={['#e65a5a']} onClick={handleClick} />
+      <button onClick={() => setType((prevState) => prevState === 'anterior' ? 'posterior' : 'anterior')}>{type}</button>
+
+        <Model type={type} data={data} onClick={handleClick} bodyColor="#ccc" />
+        {/* <ProductionModel type="posterior" data={data} highlightedColors={['#e65a5a']} onClick={handleClick} /> */}
       </div>
       <button onClick={addNewExerciseTest} style={styles.button}>Add pec deck chest exercise</button>
       <button onClick={() => setExampleState(!exampleState)}>Re-render parent</button>
